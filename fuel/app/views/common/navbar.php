e5b6c4ae2cb2ea5be096a81c14583afdcdfdd1de
Nakamura Yukinori <Nakamura Yukinori@YUKINORI-PC>
2015-05-25 01:06:54 +0900

    サークル紹介追加。

--- fuel/app/views/common/navbar.php
+++ fuel/app/views/common/navbar.php
@@ -2,6 +2,7 @@
     <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
             <li id="nav_top"><a href="/top">トップページ</a></li>
+            <li id="nav_introduce"><a href="/introduce">サークル紹介</a></li>
             <li id="nav_staff"><a href="#">メンバー紹介</a></li>
             <li id="nav_story"><a href="#">ストーリー</a></li>
             <li id="nav_product"><a href="#">製品情報</a></li>
